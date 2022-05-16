49a9f36492b609a8cb07fe3730693cf365b02d8c
@manuelduarte077 <carlosd99m@gmail.com>
2022-05-16 08:27:28 -0600

    chore: inital commit

---
+++ Devote/DevoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by Manuel Duarte on 16/5/22.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
